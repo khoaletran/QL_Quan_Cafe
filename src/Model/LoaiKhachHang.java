72f9b050ee5f98cc263e6403d6759c5c9ac00002
khoaletran <Khoaletran709@gmail.com>
2025-03-22 16:20:28 +0700

    LoaiKhachHang

---
+++ src/Model/LoaiKhachHang.java
@@ -0,0 +1,54 @@
+package Model;
+
+public class LoaiKhachHang {
+	private String maLKH ; 
+	private String tenLKH;
+	private int giamGia;
+	
+	public LoaiKhachHang() {}
+	
+	/**
+     * Constructor để tạo một loại khách hàng.
+     * 
+     * @param maLKH    Mã loại khách hàng
+     * @param tenLKH   Tên loại khách hàng
+     * @param giamGia  Phần trăm giảm giá áp dụng cho khách hàng
+     */
+	public LoaiKhachHang(String maLKH,String tenLKH,int giamGia) {
+			setMaLKH(maLKH);
+			setTenLKH(tenLKH);
+			setGiamGia(giamGia);
+	}
+	
+	
+
+	public String getMaLKH() {
+		return maLKH;
+	}
+
+	public void setMaLKH(String maLKH) {
+		this.maLKH = maLKH;
+	}
+
+	public String getTenLKH() {
+		return tenLKH;
+	}
+
+	public void setTenLKH(String tenLKH) {
+		this.tenLKH = tenLKH;
+	}
+
+	public int getGiamGia() {
+		return giamGia;
+	}
+
+	public void setGiamGia(int giamGia) {
+		this.giamGia = giamGia;
+	}
+
+	@Override
+	public String toString() {
+		return "loaiKhachHang [maLKH=" + maLKH + ", tenLKH=" + tenLKH + ", giamGia=" + giamGia + "]";
+	}
+	
+}
