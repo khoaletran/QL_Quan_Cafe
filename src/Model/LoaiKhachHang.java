80d98f07a52dca08010144806801a76678675834
Trần Lê Khoa <86742521+khoaletran@users.noreply.github.com>
2025-04-08 15:17:15 +0700

    ràng buộc loại khách hàng và khách hàng

--- src/Model/LoaiKhachHang.java
+++ src/Model/LoaiKhachHang.java
@@ -27,7 +27,7 @@ public class LoaiKhachHang {
         return maLKH;
     }
 
-    // Có thể set nếu cần gán từ DB ra object
+    // Có thể set nếu cần gán từ DB ra object a
     protected void setMaLKH(String maLKH) {
     	if(maLKH == null || !maLKH.matches("^LKH\\d{6}$")) {
     		throw new IllegalArgumentException("Mã loại khách hàng sai định dạng");
